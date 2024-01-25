8d951d7ec7c65a441e8834162a619e9871d5e478
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-01-25 14:54:34 -0700

    Made correction to the database

---
+++ Education/Migrations/20240125214803_RenameEnrollmentDeadlineColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Education.Migrations
+{
+	public partial class RenameEnrollmentDeadlineColumn : Migration
+	{
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.RenameColumn(
+				name: "enrollment_deadling",
+				table: "Courses",
+				newName: "enrollment_deadline"
+				);
+
+		}
+
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.RenameColumn(
+				name: "enrollment_deadline",
+				table: "Courses",
+				newName: "enrollment_deadling"
+				);
+		}
+	}
+}
