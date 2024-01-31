dc2971b64f2db5455a4d221e71ea67fde1f8a97a
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-01-31 15:09:08 -0700

    notes and research

--- ETL/Extract/Services/ISTCServiceInterface.cs
+++ ETL/Extract/Services/ISTCServiceInterface.cs
@@ -9,6 +9,14 @@ namespace ETL.Extract.Services
 {
 	internal interface ISTCServiceInterface
 	{
+		/// <summary>
+		/// This is a quick test I made to check if everything is hooked up okay.
+		/// If you're not me, and you need a simple example of using Interfaces,
+		/// and Direct Injection, feel free to use this method. 
+		/// </summary>
+		/// <returns>
+		///	 A <see cref="List{T}"/> of the first five records in <see cref="TblSchoolHistory"/>
+		/// </returns>
 		List<TblSchoolHistory> GetFirstFiveRecords();
 	}
 }
--- Education/Documents/SQL/GlennsSFQs/tblSchoolEnroll.sql
+++ Education/Documents/SQL/GlennsSFQs/tblSchoolEnroll.sql
@@ -1,13 +1,13 @@
-
-SELECT tblSchoolEnroll. *
-FROM tblSchoolEnroll
+-- This returns 11290 rows 
+--SELECT tblSchoolEnroll. *
+--FROM tblSchoolEnroll
 
 -- This returns 3286 rows 
 --SELECT DISTINCT LastName, FirstName
 --FROM tblSchoolEnroll
 
 
--- From Glenn's stupid fucking Coldfusion app.
+-- From Glenn's app.
 
 -- He called this recordset1
 -- This returned 34 rows for Abegglen of classes she couldn't all have possibly attended 
@@ -38,8 +38,6 @@ FROM tblSchoolEnroll
 --    )
 			
 
--- In this 
-
 -- He called this recordset2, 
 
 --SELECT     tblSchoolEnroll.*
@@ -48,4 +46,54 @@ FROM tblSchoolEnroll
 --					  tblSchoolEnroll.FirstName = 'DIANE' AND 
 --            		  tblSchoolEnroll.EmailAddr = 'dabegglen@co.fremont.id.us'  
 --order by	tblSchoolEnroll.DateRegistered DESC
+
+
+-- Apprently this is the record for everyone who has ever enrolled in the class. 
+-- I'm still unclear about how we track attendence for each student. But I'll get to that later for now 
+-- I'm gonna try and tame this bullshit. 
+
+-- Start with unique Last Names 
+--SELECT DISTINCT LastName 
+--FROM tblSchoolEnroll
+-- This is 2227 rows 
+
+-- Lets do last and first names 
+--SELECT DISTINCT LastName, FirstName
+--FROM tblSchoolEnroll
+-- This returns 3,286 Rows 
+
+-- Now lets do Glenn's stupid key 
+--SELECT DISTINCT LastName, FirstName, EmailAddr
+--FROM tblSchoolEnroll
+-- This returns 4300 rows
+-- But I can see there are many duplicate records, so when someone gets married or god forbid
+-- change their email you get another fuckin row. 
+
+-- So I can know what everyone's distinct last and first name is then 
+
+--SELECT DISTINCT LastName, FirstName
+--FROM tblSchoolEnroll
+
+-- Let try and make a table of unique Names and group that information into another table with a key 
+
+
+---- Create tblUniqueNames table
+--CREATE TABLE tblUniqueNames (
+--    UniqueID INT PRIMARY KEY AUTO_INCREMENT,
+--    LastName VARCHAR(255),
+--    FirstName VARCHAR(255)
+--);
+
+---- Insert distinct names into tblUniqueNames
+--INSERT INTO tblUniqueNames (LastName, FirstName)
+--SELECT DISTINCT LastName, FirstName
+--FROM tblSchoolEnroll;
+
+---- Create a query to retrieve rows from tblSchoolEnroll with unique names
+--SELECT tblSchoolEnroll.*
+--FROM tblSchoolEnroll
+--JOIN tblUniqueNames ON tblSchoolEnroll.LastName = tblUniqueNames.LastName
+--                   AND tblSchoolEnroll.FirstName = tblUniqueNames.FirstName;
+
+
 
