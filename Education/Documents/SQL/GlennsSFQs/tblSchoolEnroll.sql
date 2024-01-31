f40dfc2a4d3b013a0bb24cf30ff075bb92915a68
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-01-31 10:10:04 -0700

    Created console application in solution

--- Education/Documents/SQL/GlennsSFQs/tblSchoolEnroll.sql
+++ Education/Documents/SQL/GlennsSFQs/tblSchoolEnroll.sql
@@ -1,6 +1,6 @@
 
---SELECT tblSchoolEnroll. *
---FROM tblSchoolEnroll
+SELECT tblSchoolEnroll. *
+FROM tblSchoolEnroll
 
 -- This returns 3286 rows 
 --SELECT DISTINCT LastName, FirstName
@@ -15,27 +15,27 @@
 -- When I add an additional filter for cSeq for 17,18, and 33, I get 3 class she could have 
 -- attened 
 
-SELECT     tblSchoolInfo.*, tblSchoolCourses.*, tblSchoolEnroll.*
-FROM         tblSchoolEnroll RIGHT OUTER JOIN
-                      tblSchoolCourses ON tblSchoolEnroll.DateSchool = tblSchoolCourses.cDateSchool AND 
-                      tblSchoolEnroll.SchoolType = tblSchoolCourses.cSchoolType AND 
-					  tblSchoolEnroll.Seq = tblSchoolCourses.cSSeq AND
-					  tblSchoolEnroll.LastName = 'Abegglen' AND 
-					  tblSchoolEnroll.FirstName = 'Diane' AND 
-            		  tblSchoolEnroll.EmailAddr = 'dabegglen@co.fremont.id.us' RIGHT OUTER JOIN
-                      tblSchoolInfo ON tblSchoolCourses.cDateSchool = tblSchoolInfo.SDateSchool AND 
-                      tblSchoolCourses.cSchoolType = tblSchoolInfo.SSchoolType AND
-					  tblSchoolCourses.cSSeq = tblSchoolInfo.SSeq 
-WHERE     (tblSchoolInfo.SSchoolType = 's') AND 
-			(tblSchoolInfo.SDateSchool = '2019-01-07') AND 
-			(tblSchoolInfo.SSeq = 1)
-			-- My doing 
-		AND (
-        -- Add condition for cSeq values 17, 18, or 33
-        tblSchoolCourses.cSeq = 17
-        OR tblSchoolCourses.cSeq = 18
-        OR tblSchoolCourses.cSeq = 33
-    )
+--SELECT     tblSchoolInfo.*, tblSchoolCourses.*, tblSchoolEnroll.*
+--FROM         tblSchoolEnroll RIGHT OUTER JOIN
+--                      tblSchoolCourses ON tblSchoolEnroll.DateSchool = tblSchoolCourses.cDateSchool AND 
+--                      tblSchoolEnroll.SchoolType = tblSchoolCourses.cSchoolType AND 
+--					  tblSchoolEnroll.Seq = tblSchoolCourses.cSSeq AND
+--					  tblSchoolEnroll.LastName = 'Abegglen' AND 
+--					  tblSchoolEnroll.FirstName = 'Diane' AND 
+--            		  tblSchoolEnroll.EmailAddr = 'dabegglen@co.fremont.id.us' RIGHT OUTER JOIN
+--                      tblSchoolInfo ON tblSchoolCourses.cDateSchool = tblSchoolInfo.SDateSchool AND 
+--                      tblSchoolCourses.cSchoolType = tblSchoolInfo.SSchoolType AND
+--					  tblSchoolCourses.cSSeq = tblSchoolInfo.SSeq 
+--WHERE     (tblSchoolInfo.SSchoolType = 's') AND 
+--			(tblSchoolInfo.SDateSchool = '2019-01-07') AND 
+--			(tblSchoolInfo.SSeq = 1)
+--			-- My doing 
+--		AND (
+--        -- Add condition for cSeq values 17, 18, or 33
+--        tblSchoolCourses.cSeq = 17
+--        OR tblSchoolCourses.cSeq = 18
+--        OR tblSchoolCourses.cSeq = 33
+--    )
 			
 
 -- In this 
