452c6dc2fb416a73e5a5a6225de0133c4a38f061
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-02-15 15:58:05 -0700

    Course Form can now be navigated to back and forth

--- EducationUI/educationui/app/components/CourseComponents/CourseForm.tsx
+++ EducationUI/educationui/app/components/CourseComponents/CourseForm.tsx
@@ -1,58 +1,102 @@
 'use client';
 import React, { FormEvent, FocusEvent, useState, useEffect } from 'react';
 import { CourseFormData } from '@/app/shared/types/sharedTypes';
+import { init } from 'next/dist/compiled/webpack/webpack';
 
 interface CourseFormProps {
     onSubmit: (formData: CourseFormData) => void;
 }
 
 const CourseForm: React.FC<CourseFormProps> = ({ onSubmit }: {onSubmit: (formData: CourseFormData) => void}) => {
-    const [titleTouched, setTitleTouched] = useState<boolean>(false);
-    const [istitleValid, setIsTitleValid] = useState<boolean>(false);
-    const [emailTouched, setEmailTouched] = useState<boolean>(false);  
-    const [isEmailValid, setIsEmailValid] = useState<boolean>(false);
-    const [instructorNameTouched, setInstructorNameTouched] = useState<boolean>(false);
-    const [isInstructorNameValid, setIsInstructorNameValid] = useState<boolean>(false);
-    const [attendanceCreditTouched, setAttendanceCreditTouched] = useState<boolean>(false);
-    const [isAttendanceCreditValid, setIsAttendanceCreditValid] = useState<boolean>(false);
+    
+    const initialFormData : CourseFormData  = {
+        title: '',
+        description: '',
+        instructorName: '',
+        instructorEmail: '',
+        attendanceCredit: 0,
+        completionCredit: 0,
+        maxAttendance: 0,
+        enrollmentDeadline: '',
+        pdf: '',
+        locationDescription: '',
+        room: '',
+        remoteLink: '',
+        addressLine1: '',
+        addressLine2: '',
+        city: '',
+        state: 'ID',
+        postalCode: ''
+    }
+
+const [formData, setFormData] = useState<CourseFormData>(() => {
+    let savedFormData;
+    if (typeof localStorage !== 'undefined') {
+        savedFormData = localStorage.getItem('courseFormData');
+    }
+    return savedFormData ? JSON.parse(savedFormData) : initialFormData;
+});
+
+    const validatEamil = (email: any) => {
+        const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+    const validateCompletionCredit = (value: string): boolean => {
+        const parsedValue = parseInt(value);
+        return parsedValue >= attendanceCredit 
+    }
+
+    const validateEnrollmentDeadline = (value: string): boolean => {
+        const selectedDate = new Date(value);
+        return selectedDate > new Date();
+    }
+
+    const [titleTouched, setTitleTouched] = useState<boolean>(initialFormData.title !== '');
+    const [istitleValid, setIsTitleValid] = useState<boolean>(initialFormData.title !== '');
+
+    const [emailTouched, setEmailTouched] = useState<boolean>(initialFormData.instructorEmail !== '');  
+    const [isEmailValid, setIsEmailValid] = useState<boolean>(validatEamil(initialFormData.instructorEmail));
+
+    const [instructorNameTouched, setInstructorNameTouched] = useState<boolean>(initialFormData.instructorName !== '');
+    const [isInstructorNameValid, setIsInstructorNameValid] = useState<boolean>(initialFormData.instructorName !== '');
+
+    const [attendanceCreditTouched, setAttendanceCreditTouched] = useState<boolean>(initialFormData.attendanceCredit !== 0);
+    const [isAttendanceCreditValid, setIsAttendanceCreditValid] = useState<boolean>(initialFormData.attendanceCredit !== 0);
+
     const [attendanceCredit, setAttendanceCredit] = useState<number>(0);
-    const [isCompletionCreditValid, setIsCompletionCreditValid] = useState<boolean>(false);
-    const [completionCreditTouched, setCompletionCreditTouched] = useState<boolean>(false);
-    const [maxAttendanceTouched, setMaxAttendanceTouched] = useState<boolean>(false);
-    const [isMaxAttendanceValid, setIsMaxAttendanceValid] = useState<boolean>(false);
-    const [enrollmentDeadlineTouched, setEnrollmentDeadlineTouched] = useState<boolean>(false);
-    const [isEnrollmentDeadlineValid, setIsEnrollmentDeadlineValid] = useState<boolean>(false);
-    const [addressLine1Touched, setAddressLine1Touched] = useState<boolean>(false);
-    const [isAddressLine1Valid, setIsAddressLine1Valid] = useState<boolean>(false);
-    const [cityTouched, setCityTouched] = useState<boolean>(false);
-    const [isCityValid, setIsCityValid] = useState<boolean>(false);
-    const [postalCodeTouched, setPostalCodeTouched] = useState<boolean>(false);
-    const [isPostalCodeValid, setIsPostalCodeValid] = useState<boolean>(false);
-    const [formData, setFormData] = useState<CourseFormData>(() => {
-        const saveFormData = localStorage.getItem('courseFormData');
-        return saveFormData ? JSON.parse(saveFormData) : {
-            title: '',
-            description: '',
-            instructorName: '',
-            instructorEmail: '',
-            attendanceCredit: '',
-            completionCredit: '',
-            maxAttendance: '',
-            enrollmentDeadline: '',
-            pdf: '',
-            locationDescription: '',
-            room: '',
-            remoteLink: '',
-            addressLine1: '',
-            addressLine2: '',
-            city: '',
-            state: 'ID',
-            postalCode: ''
-        };
-    });
+
+    const [completionCreditTouched, setCompletionCreditTouched] = useState<boolean>(initialFormData.completionCredit !== 0);
+    const [isCompletionCreditValid, setIsCompletionCreditValid] = useState<boolean>(initialFormData.completionCredit >= attendanceCredit);
+
+    const [maxAttendanceTouched, setMaxAttendanceTouched] = useState<boolean>(initialFormData.maxAttendance !== 0);
+    const [isMaxAttendanceValid, setIsMaxAttendanceValid] = useState<boolean>(initialFormData.maxAttendance !== 0);
+
+    const [enrollmentDeadlineTouched, setEnrollmentDeadlineTouched] = useState<boolean>(initialFormData.enrollmentDeadline !== '');
+    const [isEnrollmentDeadlineValid, setIsEnrollmentDeadlineValid] = useState<boolean>(validateEnrollmentDeadline(initialFormData.enrollmentDeadline));
+
+    const [addressLine1Touched, setAddressLine1Touched] = useState<boolean>(initialFormData.addressLine1 !== '');
+    const [isAddressLine1Valid, setIsAddressLine1Valid] = useState<boolean>(initialFormData.addressLine1 !== '');
+
+    const [cityTouched, setCityTouched] = useState<boolean>(initialFormData.city !== '');
+    const [isCityValid, setIsCityValid] = useState<boolean>(initialFormData.city !== '');
+
+    const [postalCodeTouched, setPostalCodeTouched] = useState<boolean>(initialFormData.postalCode !== '');
+    const [isPostalCodeValid, setIsPostalCodeValid] = useState<boolean>(initialFormData.postalCode !== '');
 
     useEffect(() => {
         localStorage.setItem('courseFormData', JSON.stringify(formData));
+
+        setIsTitleValid(formData.title !== '');
+        setIsEmailValid(validatEamil(formData.instructorEmail));
+        setIsInstructorNameValid(formData.instructorName !== '');
+        setIsAttendanceCreditValid(formData.attendanceCredit !== 0);
+        setIsCompletionCreditValid(validateCompletionCredit(formData.completionCredit.toString()));
+        setIsMaxAttendanceValid(formData.maxAttendance !== 0);
+        setIsEnrollmentDeadlineValid(validateEnrollmentDeadline(formData.enrollmentDeadline));
+        setIsAddressLine1Valid(formData.addressLine1 !== '');
+        setIsCityValid(formData.city !== '');
+        setIsPostalCodeValid(formData.postalCode !== '');
     }, [formData]);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>): void => {
@@ -115,11 +159,6 @@ const CourseForm: React.FC<CourseFormProps> = ({ onSubmit }: {onSubmit: (formDat
         setIsEmailValid(isValid);
     }
 
-    const validatEamil = (email: any) => {
-        const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
-    }
-
     const handleInstructorNameBlur = (event: FocusEvent<HTMLInputElement, Element>): void => {
         setInstructorNameTouched(true);
         const isValid = event.target.value.length > 0;
@@ -140,11 +179,6 @@ const CourseForm: React.FC<CourseFormProps> = ({ onSubmit }: {onSubmit: (formDat
         setIsCompletionCreditValid(isValid);
     }
 
-    const validateCompletionCredit = (value: string): boolean => {
-        const parsedValue = parseInt(value);
-        return parsedValue >= attendanceCredit 
-    }
-
     const handleMaxAttendanceBlur = (event: FocusEvent<HTMLInputElement, Element>): void => {
         setMaxAttendanceTouched(true);
         const isValid = event.target.value.length > 0;
@@ -155,11 +189,6 @@ const CourseForm: React.FC<CourseFormProps> = ({ onSubmit }: {onSubmit: (formDat
         setEnrollmentDeadlineTouched(true);
         const isValid = validateEnrollmentDeadline(event.target.value);
         setIsEnrollmentDeadlineValid(isValid);
-    }
-
-    const validateEnrollmentDeadline = (value: string): boolean => {
-        const selectedDate = new Date(value);
-        return selectedDate > new Date();
     }
 
     const handleAddressLine1Blur = (event: FocusEvent<HTMLInputElement, Element>): void => {
@@ -180,9 +209,21 @@ const CourseForm: React.FC<CourseFormProps> = ({ onSubmit }: {onSubmit: (formDat
         setIsPostalCodeValid(isValid);
     }
 
-    const isFormValid = istitleValid && isEmailValid && isInstructorNameValid && isAttendanceCreditValid && isCompletionCreditValid && isMaxAttendanceValid && isEnrollmentDeadlineValid && isAddressLine1Valid && isCityValid  && isPostalCodeValid;
+    const isFormValid = istitleValid &&
+        isEmailValid && 
+        isInstructorNameValid && 
+        isAttendanceCreditValid &&
+        isCompletionCreditValid && 
+        isMaxAttendanceValid &&
+        isEnrollmentDeadlineValid &&
+        isAddressLine1Valid &&
+        isCityValid && 
+        isPostalCodeValid;
 
     return (
+        <>
+        <button onClick={() => localStorage.clear()}>Clear Local Storage</button>
+
         <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
 
             <div className="mb-4">
@@ -487,7 +528,6 @@ const CourseForm: React.FC<CourseFormProps> = ({ onSubmit }: {onSubmit: (formDat
                     <select
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         id="state"
-                        defaultValue="ID" // Default value set to Idaho
                         onChange = {handleChange}
                         value = {formData.state}
                     >
@@ -577,6 +617,7 @@ const CourseForm: React.FC<CourseFormProps> = ({ onSubmit }: {onSubmit: (formDat
                 {!isFormValid && <p className="text-red-500 text-xs italic w-1/2">Please fill out all required fields.</p>}
             </div>
         </form>
+        </>
   )
 }
 
