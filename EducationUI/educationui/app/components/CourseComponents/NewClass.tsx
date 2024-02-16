71ad4f7f4259e52fd150f181df1ec5d79ff6d5f3
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-02-15 17:25:18 -0700

    NewClass now has delete

--- EducationUI/educationui/app/components/CourseComponents/NewClass.tsx
+++ EducationUI/educationui/app/components/CourseComponents/NewClass.tsx
@@ -1,27 +1,35 @@
-'use client';
+import React, { useState } from 'react';
 
-interface ClassFromProps {
-}
+interface ClassFromProps {}
 
-const NewClass : React.FC<ClassFromProps> = () => {
-    return (
-        <div className = "bg-white shadow-md rounded-md p-4">
-           <label 
-                htmlFor = "classDate"
-                className="block text-gray-700 text-sm font-bold mb-2">
-                    Class Date
+const NewClass: React.FC<ClassFromProps> = () => {
+    const [isVisible, setIsVisible] = useState(true);
+
+    const handleRemoveClick = () => {
+        setIsVisible(false);
+    };
+
+    return isVisible ? (
+        <div className="bg-white shadow-md rounded-md p-4 relative">
+            <button
+                className="absolute top-0 right-0 mr-2 mt-2 bg-red-500 text-white px-2 py-1 rounded"
+                onClick={handleRemoveClick}
+            >
+                Remove
+            </button>
+            <label htmlFor="classDate" className="block text-gray-700 text-sm font-bold mb-2">
+                Class Date
             </label>
-            <input 
+            <input
                 type="date"
                 name="classDate"
                 id="classDate"
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                defaultValue = {new Date().toISOString().split('T')[0]}
-                min = {new Date().toISOString().split('T')[0]}
-            />      
+                defaultValue={new Date().toISOString().split('T')[0]}
+                min={new Date().toISOString().split('T')[0]}
+            />
         </div>
+    ) : null;
+};
 
-    );
-}
-
-export default NewClass
+export default NewClass;
