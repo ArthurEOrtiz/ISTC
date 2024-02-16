6326e8aa60e3d8c1331be34bea83434b9e37c8ee
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-02-16 12:26:53 -0700

    refactored delete functionality.

--- EducationUI/educationui/app/components/CourseComponents/NewClass.tsx
+++ EducationUI/educationui/app/components/CourseComponents/NewClass.tsx
@@ -1,15 +1,18 @@
 // NewClass.tsx
+import { time } from 'console';
 import React, { useState } from 'react';
 
 interface NewClassProps {
     scheduleDate: Date; // Prop to receive the class date
     onDelete: () => void; // Prop to receive the delete event
     onDateChange: (date: Date) => void; // Prop to receive the date change event
+    onStartTimeChange: (time: string) => void; // Prop to receive the start time change event
 }
 
-const NewClass: React.FC<NewClassProps> = ({ scheduleDate, onDelete, onDateChange }) => {
-    const [isVisible, setIsVisible] = useState(true);
 
+const NewClass: React.FC<NewClassProps> = ({ scheduleDate, onDelete, onDateChange, onStartTimeChange }) => {
+    // const [isVisible, setIsVisible] = useState<boolean>(true);
+    const [startTime, setStartTime] = useState<string>(); // Set the default start time to 09:00
     const handleRemoveClick = () => {
         onDelete();
     };
@@ -18,8 +21,14 @@ const NewClass: React.FC<NewClassProps> = ({ scheduleDate, onDelete, onDateChang
         onDateChange(new Date(event.target.value));
     }
 
+    const handleStartTimeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const startTime = event.target.value;
+        onStartTimeChange(startTime);
+        setStartTime(startTime);
+    }
 
-    return isVisible ? (
+
+    return (
         <div className="bg-white shadow-md rounded-md p-4 relative">
             <button
                 className="absolute top-0 right-0 mr-2 mt-1 bg-red-500 text-white px-2 py-1 rounded"
@@ -52,6 +61,8 @@ const NewClass: React.FC<NewClassProps> = ({ scheduleDate, onDelete, onDateChang
                         name="startTime"
                         id="startTime"
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        defaultValue="09:00"
+                        onChange={handleStartTimeChange}
                     />
                 </div>
                 <div className="w-full px-3 mb-6 md:w-1/3 md:mb-0">
@@ -63,11 +74,12 @@ const NewClass: React.FC<NewClassProps> = ({ scheduleDate, onDelete, onDateChang
                         name="endTime"
                         id="endTime"
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        defaultValue="17:00"
                     />
                 </div>
             </div>
         </div>
-    ) : null;
+    );
 };
 
 export default NewClass;
