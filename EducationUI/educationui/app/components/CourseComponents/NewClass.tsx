9a47f544f52903c8734bd78720ad13e563c595db
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-02-15 17:21:32 -0700

    Refactor newClass

--- EducationUI/educationui/app/components/CourseComponents/NewClass.tsx
+++ EducationUI/educationui/app/components/CourseComponents/NewClass.tsx
@@ -1,11 +1,9 @@
 'use client';
-import { CourseFormData } from '@/app/shared/types/sharedTypes';
 
 interface ClassFromProps {
-    courseFormData: CourseFormData | null;
 }
 
-const NewClass : React.FC<ClassFromProps> = ({courseFormData}) => {
+const NewClass : React.FC<ClassFromProps> = () => {
     return (
         <div className = "bg-white shadow-md rounded-md p-4">
            <label 
@@ -18,7 +16,7 @@ const NewClass : React.FC<ClassFromProps> = ({courseFormData}) => {
                 name="classDate"
                 id="classDate"
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                value = {new Date().toISOString().split('T')[0]}
+                defaultValue = {new Date().toISOString().split('T')[0]}
                 min = {new Date().toISOString().split('T')[0]}
             />      
         </div>
