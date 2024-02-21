7df280455580c6996f53f9ed5bb4aeebdc666bce
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-02-21 12:41:27 -0700

    refactored CourseCard to accept Course type

--- EducationUI/educationui/app/components/CourseComponents/ClassForm.tsx
+++ EducationUI/educationui/app/components/CourseComponents/ClassForm.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from 'react';
-import { CourseFormData } from '@/app/shared/types/sharedTypes';
+import React, { useEffect, useRef, useState } from 'react';
+import { Course, CourseFormData } from '@/app/shared/types/sharedTypes';
 import CourseCard from './CourseCard';
 import NewClass from './NewClass';
 
@@ -13,6 +13,33 @@ interface ClassFormProps {
 }
 
 const ClassForm: React.FC<ClassFormProps> = ({ courseFormData, classes, onDeleteClass, onClassDateChange, onStartTimeChange, onEndTimeChange }) => {
+    const [course, setCourse] = useState<Course >(
+        {
+            courseId: null,
+            title: '',
+            description: '',
+            attendanceCredit: 0,
+            completionCredit: 0,
+            maxAttendance: 0,
+            enrollmentDeadline: '',
+            instructorName: '',
+            instructorEmail: '',
+            pdf: '',
+            location: {
+                locationId: null,
+                description: '',
+                room: '',
+                remoteLink: '',
+                addressLine1: '',
+                addressLine2: '',
+                city: '',
+                state: '',
+                postalCode: ''
+            },
+            classes: []
+        }
+    );
+
 
     useEffect(() => {
         // Scroll to the bottom of the window after classes change
@@ -22,10 +49,44 @@ const ClassForm: React.FC<ClassFormProps> = ({ courseFormData, classes, onDelete
         });
     }, [classes]);
 
+    useEffect(() => {
+        if (courseFormData) {
+            setCourse({
+                courseId: null,
+                title: courseFormData.title,
+                description: courseFormData.description,
+                attendanceCredit: courseFormData.attendanceCredit,
+                completionCredit: courseFormData.completionCredit,
+                maxAttendance: courseFormData.maxAttendance,
+                enrollmentDeadline: courseFormData.enrollmentDeadline,
+                instructorName: courseFormData.instructorName,
+                instructorEmail: courseFormData.instructorEmail,
+                pdf: courseFormData.pdf,
+                location: {
+                    locationId: null,
+                    description: courseFormData.locationDescription,
+                    room: courseFormData.room,
+                    remoteLink: courseFormData.remoteLink,
+                    addressLine1: courseFormData.addressLine1,
+                    addressLine2: courseFormData.addressLine2,
+                    city: courseFormData.city,
+                    state: courseFormData.state,
+                    postalCode: courseFormData.postalCode
+                },
+                classes: classes.map(classItem => ({
+                    classId: null,
+                    courseId: null,
+                    ScheduleStart: classItem.scheduleDate.toISOString(),
+                    ScheduleEnd: classItem.scheduleDate.toISOString()
+                }))
+            });
+        }
+    }, []);
+
     return (
         <div>
             <div className="mb-3">
-                <CourseCard courseFormData={courseFormData} />
+                <CourseCard course={course} />
             </div>
             <div>
                 {classes.map((classItem, index) => (
--- EducationUI/educationui/app/components/CourseComponents/CourseCard.tsx
+++ EducationUI/educationui/app/components/CourseComponents/CourseCard.tsx
@@ -1,62 +1,62 @@
-import { CourseFormData } from "@/app/shared/types/sharedTypes";
+import { Course } from "@/app/shared/types/sharedTypes";
 
 interface CourseCardProps {
-    courseFormData: CourseFormData | null;  
+    course : Course; 
 };
 
 
-const CourseCard : React.FC<CourseCardProps> = ({courseFormData}) => {
+const CourseCard : React.FC<CourseCardProps> = ({course}) => {
     return(
         <div className="bg-white shadow-md rounded-md p-4">
             <div className="mb-2">
-                <h1 className="text-xl font-bold">{courseFormData?.title}</h1>
-                <p><strong>Description:</strong> {courseFormData?.description}</p>
+                <h1 className="text-xl font-bold">{course?.title}</h1>
+                <p><strong>Description:</strong> {course?.description}</p>
             </div>
             <div className="flex flex-wrap -mx-1">
                 <div className="w-full sm:w-1/2 px-1 mb-2">
-                    <p><strong>Instructor:</strong> {courseFormData?.instructorName}</p>
+                    <p><strong>Instructor:</strong> {course?.instructorName}</p>
                 </div>
                 <div className="w-full sm:w-1/2 px-1 mb-2">
-                    <p><strong>Email:</strong> {courseFormData?.instructorEmail}</p>
+                    <p><strong>Email:</strong> {course?.instructorEmail}</p>
                 </div>
                 <div className="w-full sm:w-1/2 px-1 mb-2">
-                    <p><strong>Attendance Credit:</strong> {courseFormData?.attendanceCredit}</p>
+                    <p><strong>Attendance Credit:</strong> {course?.attendanceCredit}</p>
                 </div>
                 <div className="w-full sm:w-1/2 px-1 mb-2">
-                    <p><strong>Completion Credit:</strong> {courseFormData?.completionCredit}</p>
+                    <p><strong>Completion Credit:</strong> {course?.completionCredit}</p>
                 </div>
                 <div className="w-full sm:w-1/2  px-1 mb-2">
-                    <p><strong>Max Attendance:</strong> {courseFormData?.maxAttendance}</p>
+                    <p><strong>Max Attendance:</strong> {course?.maxAttendance}</p>
                 </div>
                 <div className="w-full sm:w-1/2  px-1 mb-2">
-                    <p><strong>Enrollment Deadline:</strong> {courseFormData?.enrollmentDeadline}</p>
+                    <p><strong>Enrollment Deadline:</strong> {course?.enrollmentDeadline}</p>
                 </div>
                 <div className="w-full px-1 mb-2">
-                    <p><strong>PDF:</strong> {courseFormData?.pdf}</p>
+                    <p><strong>PDF:</strong> {course?.pdf}</p>
                 </div>
                 <div className="w-full px-1 mb-2">
-                    <p><strong>Location:</strong> {courseFormData?.locationDescription}</p>
+                    <p><strong>Location:</strong> {course?.location?.description}</p>
                 </div>
                 <div className="w-full px-1 mb-2">
-                    <p><strong>Room:</strong> {courseFormData?.room}</p>
+                    <p><strong>Room:</strong> {course?.location?.room}</p>
                 </div>
                 <div className="w-full px-1 mb-2">
-                    <p><strong>Remote Link:</strong> {courseFormData?.remoteLink}</p>
+                    <p><strong>Remote Link:</strong> {course?.location?.remoteLink}</p>
                 </div>
                 <div className="w-full px-1 mb-2">
-                    <p><strong>Address Line 1:</strong> {courseFormData?.addressLine1}</p>
+                    <p><strong>Address Line 1:</strong> {course?.location?.addressLine1}</p>
                 </div>
                 <div className="w-full px-1 mb-2">
-                    <p><strong>Address Line 2:</strong> {courseFormData?.addressLine2}</p>
+                    <p><strong>Address Line 2:</strong> {course?.location?.addressLine2}</p>
                 </div>
                 <div className="w-full sm:w-1/2 px-1 mb-2">
-                    <p><strong>City:</strong> {courseFormData?.city}</p>
+                    <p><strong>City:</strong> {course?.location?.city}</p>
                 </div>
                 <div className="w-full sm:w-1/2 px-1 mb-2">
-                    <p><strong>State:</strong> {courseFormData?.state}</p>
+                    <p><strong>State:</strong> {course?.location?.state}</p>
                 </div>
                 <div className="w-full sm:w-1/2 px-1 mb-2">
-                    <p><strong>Zip:</strong> {courseFormData?.postalCode}</p>
+                    <p><strong>Zip:</strong> {course?.location?.postalCode}</p>
                 </div>
             </div>
         </div>
