c2851f49fb2a18ffaa9ffe0407d41d2133848746
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-03-14 14:59:51 -0600

    working through a mock up
    exploring one way to register users after sign up or sign in with clerk.

---
+++ EducationUI/educationui/app/register-user/page.tsx
@@ -0,0 +1,9 @@
+const RegisterUserPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Register User</h1>
+        </div>
+    );
+}
+
+export default RegisterUserPage;
--- EducationUI/educationui/app/sign-in/[[...sign-in]]/page.tsx
+++ EducationUI/educationui/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,12 @@
+'use client';
+
 import { SignIn } from "@clerk/nextjs";
  
 export default function Page() {
+
   return(
     <div className="flex flex-col items-center justify-center h-screen">
-      <SignIn />
+      <SignIn  />
     </div>
   )
 }
