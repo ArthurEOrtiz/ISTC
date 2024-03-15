0e11f992ce2af7076613cc6019e5f51a59fd64c7
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-03-15 16:37:23 -0600

    working user info card
    Getting the mock up of the user info card.

--- EducationUI/educationui/app/components/UserComponents/UserDashboard.tsx
+++ EducationUI/educationui/app/components/UserComponents/UserDashboard.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import { getUserByClerkId } from "@/Utilities/api";
 import { User } from '@/app/shared/types/sharedTypes';
 import Loading from '@/app/shared/Loading';
+import { User } from '@clerk/nextjs/server';
+import UserInfoCard from './UserInfoCard';
 
 interface UserDashboardProps {
     clerkId: string;
@@ -24,37 +26,19 @@ const UserDashboard: React.FC<UserDashboardProps> = ({clerkId}) => {
         fetchUser();
     }, [clerkId]);
 
+    const handleApply = (user: User) => {
+        console.log(user);
+    };
+
     if (!user) {
         return <Loading />
     }
-    
 
     return (
         <div>
             <h1 className="p-2 text-3xl text-center font-bold">User Dashboard</h1>
-            <div>
-                <p>First Name: {user.firstName}</p>
-                <p>Last Name: {user.lastName}</p>
-                <p>Middle Name: {user.middleName}</p>
-                <p>Email: {user.email}</p>
-                <p>Employer: {user.employer}</p>
-                <p>Job Title: {user.jobTitle}</p>
-                <p>Is Admin: {user.isAdmin ? 'Yes' : 'No'}</p>
-                <p>Is Student: {user.isStudent ? 'Yes' : 'No'}</p>
-                {user.student && (
-                    <div>
-                        <p>Student ID: {user.student.studentId}</p>
-                        <p>Accumulated Credit: {user.student.accumulatedCredit}</p>
-                        <p>Appraisal Certified: {user.student.appraisalCertified ? 'Yes' : 'No'}</p>
-                        <p>Mapping Certified: {user.student.mappingCertified ? 'Yes' : 'No'}</p>
-                    </div>
-                )}
-                <p>Phone: {user.contact.phone}</p>
-                <p>Address Line 1: {user.contact.addressLine1}</p>
-                <p>Address Line 2: {user.contact.addressLine2}</p>
-                <p>City: {user.contact.city}</p>
-                <p>State: {user.contact.state}</p>
-                <p>Zip: {user.contact.zip}</p>
+            <div className='flex justify-center mt-4'>
+                <UserInfoCard user={user} onApply={handleApply} />
             </div>
         </div>
     );
---
+++ EducationUI/educationui/app/components/UserComponents/UserInfoCard.tsx
@@ -0,0 +1,49 @@
+import { User } from "@/app/shared/types/sharedTypes";
+
+interface UserInfoCardProps {
+    user: User;
+    onApply: (user: User) => void;
+}
+
+const UserInfoCard: React.FC<UserInfoCardProps> = ({ user, onApply }) => {
+    return (
+        <div className="bg-white shawdow-md rounded-xl p-4">
+            <h1 className="text-2xl text-center font-bold">{user.firstName} {user.lastName}</h1>
+            <div>
+                <p className="text-center">{user.employer} | {user.jobTitle}</p>
+                <p className="text-center">{user.email}</p>
+            </div>
+
+            <div className="mt-4">
+                {user.student  && (
+                    <div>
+                        <div className="flex">
+                            <p className="text-base font-bold mr-2">Student Id:</p>
+                            <p>{user.student.studentId}</p>
+                        </div>
+
+                        <div className="flex">
+                            <p className="text-base font-bold mr-2">Accumulated Credits:</p>
+                            <p>{user.student.accumulatedCredit}</p>
+                        </div>
+
+                        <div className="flex">
+                            <p className="text-base font-bold mr-2">Appriasal Certified</p>
+                            <p>{user.student.appraisalCertified ? "Yes" : "No"}</p>
+                        </div>
+
+                        <div className="flex">
+                            <p className="text-base font-bold mr-2">Mapping Certified</p>
+                            <p>{user.student.mappingCertified ? "Yes" : "No"}</p>
+                        </div>
+                    </div>
+                )}
+            </div>
+            
+            
+
+        </div>
+    );
+}
+
+export default UserInfoCard;
