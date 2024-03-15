862566ed9ec5d590dc06a0c74cbda6eb2e396fdd
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-03-15 17:06:06 -0600

    Working user info card

--- EducationUI/educationui/app/components/UserComponents/UserInfoCard.tsx
+++ EducationUI/educationui/app/components/UserComponents/UserInfoCard.tsx
@@ -7,7 +7,7 @@ interface UserInfoCardProps {
 
 const UserInfoCard: React.FC<UserInfoCardProps> = ({ user, onApply }) => {
     return (
-        <div className="bg-white shawdow-md rounded-xl p-4">
+        <div className="bg-white shawdow-md rounded-xl p-4 w-1/2">
             <h1 className="text-2xl text-center font-bold">{user.firstName} {user.lastName}</h1>
             <div>
                 <p className="text-center">{user.employer} | {user.jobTitle}</p>
@@ -38,6 +38,41 @@ const UserInfoCard: React.FC<UserInfoCardProps> = ({ user, onApply }) => {
                         </div>
                     </div>
                 )}
+            </div>
+
+            <div className="mt-4">
+                    {user.contact && (
+                        <div>
+                            <div className="flex">
+                                <p className="text-base font-bold mr-2">Phone:</p>
+                                <p>{user.contact.phone}</p>
+                            </div>
+                            <div className="flex">
+                                <p className="text-base font-bold mr-2">Address:</p>
+                                <p>{user.contact.addressLine1}</p>
+                                <p>{user.contact.addressLine2}</p>
+                            </div>
+
+                            <div className="flex justify-start gap-x-8">
+                                <div className="flex">
+                                    <p className="text-base font-bold mr-2">City:</p>
+                                    <p>{user.contact.city}</p>
+                                </div>
+                              
+                                <div className="flex">
+                                    <p className="text-base font-bold mr-2">State:</p>
+                                    <p>{user.contact.state}</p>
+                                </div>
+                                
+                                <div className="flex">
+                                    <p className="text-base font-bold mr-2">Zip:</p>
+                                    <p>{user.contact.zip}</p>
+                                </div>
+
+                            </div>
+                        </div>
+                    )}
+            
             </div>
             
             
