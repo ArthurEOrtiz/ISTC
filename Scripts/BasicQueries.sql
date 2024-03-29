25e2246c2a30f082d23351c3643f58f823a9d5e0
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-03-29 09:37:19 -0600

    Create BasicQueries.sql
    basic queries used to test the database

---
+++ Scripts/BasicQueries.sql
@@ -0,0 +1,27 @@
+SELECT *
+FROM Users;
+
+--UPDATE Users
+--SET IsAdmin = 1;
+
+SELECT *
+FROM Contact;
+
+SELECT *
+FROM Students;
+
+SELECT *
+FROM Classes;
+
+SELECT *
+FROM Attendance;
+--WHERE class_id IN (147, 148, 151, 152)
+
+
+--UPDATE Attendance
+--SET Attended = 1
+--WHERE Attendance.AttendanceId = 1024
+
+
+SELECT *
+FROM Courses;
