61e6a8e891cba0bc2c76b0ac350545ddca78b7b8
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-04-18 13:20:36 -0600

    Update sharedTypes.ts
    added exam type

--- EducationUI/educationui/app/shared/types/sharedTypes.ts
+++ EducationUI/educationui/app/shared/types/sharedTypes.ts
@@ -1,88 +1,102 @@
+export interface Attendance {
+  attendanceId: number;
+  attended: boolean;
+  studentId: number;
+  classId: number;
+}
+
+export interface Class {
+  classId: number;
+  courseId: number;
+  scheduleStart: string;
+  scheduleEnd: string;
+  attendances: Attendance[];
+}
+
+export interface Contact {
+  contactId: number;
+  userId: number;
+  phone: string | null;
+  addressLine1: string | null;
+  addressLine2: string | null;
+  state: string | null;
+  city: string | null;
+  zip: string | null;
+}
+
+export interface Course {
+  courseId: number;
+  title: string;
+  description: string | null;
+  attendanceCredit: number;
+  hasExam: boolean;
+  examCredit: number | null;
+  maxAttendance: number;
+  enrollmentDeadline: Date;
+  instructorName: string | null;
+  instructorEmail: string;
+  pdf: string | null; 
+  locationId: number;
+  location: Location;
+  topics: Topic[] | null;
+  classes: Class[];
+}
+
+export interface Exam {
+  examId: number;
+  courseId: number;
+  userId: number;
+  HasPassed: boolean;
+}
 
 export interface Location {
-    locationId: number | null;
-    description: string | null;
-    room: string | null;
-    remoteLink: string | null;
-    addressLine1: string;
-    addressLine2: string | null;
-    city: string;
-    state: string;
-    postalCode: string;
-  }
+  locationId: number | null;
+  description: string | null;
+  room: string | null;
+  remoteLink: string | null;
+  addressLine1: string;
+  addressLine2: string | null;
+  city: string;
+  state: string;
+  postalCode: string;
+}
+
+export interface Student {
+  studentId: number;
+  userId: number;
+  accumulatedCredit: number;
+  appraisalCertified: boolean;
+  mappingCertified: boolean;
+  attendances: Attendance[];
+}
+
+export interface Topic {
+  topicId: number | null;
+  title: string;
+  description: string;
+  courses: Course[] | null;
+}
+
+export interface User {
+  userId: number;
+  clerkId: string | null;
+  firstName: string;
+  lastName: string;
+  middleName: string | null;
+  email: string;
+  employer: string;
+  jobTitle: string; 
+  isAdmin: boolean;
+  isStudent: boolean;
+  student : Student | null;
+  contact: Contact;
+}
   
-export interface Class {
-    classId: number;
-    courseId: number;
-    scheduleStart: string;
-    scheduleEnd: string;
-    attendances: Attendance[];
-  }
+
+
+
+
+
+
   
-export interface Course {
-    courseId: number;
-    title: string;
-    description: string | null;
-    attendanceCredit: number;
-    hasExam: boolean;
-    examCredit: number | null;
-    maxAttendance: number;
-    enrollmentDeadline: Date;
-    instructorName: string | null;
-    instructorEmail: string;
-    pdf: string | null; 
-    locationId: number;
-    location: Location;
-    topics: Topic[] | null;
-    classes: Class[];
-  }
-  
-  export interface Topic {
-    topicId: number | null;
-    title: string;
-    description: string;
-    courses: Course[] | null;
-  }
 
-  export interface Attendance {
-    attendanceId: number;
-    attended: boolean;
-    studentId: number;
-    classId: number;
-    
-  }
-
-  export interface User {
-    userId: number;
-    clerkId: string | null;
-    firstName: string;
-    lastName: string;
-    middleName: string | null;
-    email: string;
-    employer: string;
-    jobTitle: string; 
-    isAdmin: boolean;
-    isStudent: boolean;
-    student : Student | null;
-    contact: Contact;
-  }
-
-  export interface Student {
-    studentId: number;
-    userId: number;
-    accumulatedCredit: number;
-    appraisalCertified: boolean;
-    mappingCertified: boolean;
-    attendances: Attendance[];
-  }
-
-  export interface Contact {
-    contactId: number;
-    userId: number;
-    phone: string | null;
-    addressLine1: string | null;
-    addressLine2: string | null;
-    state: string | null;
-    city: string | null;
-    zip: string | null;
-  }
