f7e4513476aafefef46b7918e6c62d61c3ab29c5
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-04-24 10:33:00 -0600

    Update ErrorModal.tsx

--- EducationUI/educationui/app/shared/modals/ErrorModal.tsx
+++ EducationUI/educationui/app/shared/modals/ErrorModal.tsx
@@ -8,8 +8,11 @@ const ErrorModal: React.FC<ErrorModalProps> = ({ title, message, onClose }) => {
     return (
         <div className="fixed inset-0 flex items-center justify-center z-10">
             <div className="absolute inset-0 bg-black opacity-50"></div>
-            <div className="bg-base-200 rounded-lg p-4 z-50">
-                <h2 className="font-bold text-lg text-error mb-4">{title}</h2>
+            <div className="bg-base-200 rounded-xl p-4 z-50">
+                <div className="flex justify-between items-baseline">
+                    <h2 className="font-bold text-lg text-error mb-4">{title}</h2>
+                    <button onClick={onClose} className="text-error font-bold">X</button>
+                </div>
                 <p className="text-lg mb-4">{message}</p>
                 <button onClick={onClose} className="btn btn-ghost dark:text-white">Close</button>
             </div>
