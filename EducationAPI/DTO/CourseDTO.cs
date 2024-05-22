f92fcbecdc5d1a40a42ef40be971c908c8512c10
Arthur Ortiz <Ortiz.Arthur.E@gmail.com>
2024-05-22 16:32:14 -0600

    Update CourseDTO.cs

--- EducationAPI/DTO/CourseDTO.cs
+++ EducationAPI/DTO/CourseDTO.cs
@@ -1,24 +1,29 @@
-﻿namespace EducationAPI.DTO
+﻿using EducationAPI.Models;
+
+namespace EducationAPI.DTO
 {
   public class CourseDTO
   {
-    public int CourseId { get; set; }
-    public string Title { get; set; } = null!;
-    public string? Description { get; set; }
-    public int AttendanceCredit { get; set; }
-    public bool HasExam { get; set; }
-    public int? ExamCredit { get; set; }
-    public int MaxAttendance { get; set; }
-    public DateTime EnrollmentDeadline { get; set; }
-    public string? InstructorName { get; set; }
-    public string? InstructorEmail { get; set; }
-    public int? PDFId { get; set; }
-    public int LocationId { get; set; }
-    public PDFDTO? PDF { get; set; }
-    public LocationDTO Location { get; set; } = new();
-    public ICollection<TopicDTO> Topics { get; set; } = [];
-    public ICollection<ClassDTO> Classes { get; set; } = [];
-    public ICollection<ExamDTO> Exams { get; set; } = [];
-    public ICollection<WaitListDTO> WaitLists { get; set; } = [];
+    public Course Course { get; set; } = new();
+    public List<int> NewClassIds { get; set; } = [];
+
+    // public int CourseId { get; set; }
+    // public string Title { get; set; } = null!;
+    // public string? Description { get; set; }
+    // public int AttendanceCredit { get; set; }
+    // public bool HasExam { get; set; }
+    // public int? ExamCredit { get; set; }
+    // public int MaxAttendance { get; set; }
+    // public DateTime EnrollmentDeadline { get; set; }
+    // public string? InstructorName { get; set; }
+    // public string? InstructorEmail { get; set; }
+    // public int? PDFId { get; set; }
+    // public int LocationId { get; set; }
+    // public PDFDTO? PDF { get; set; }
+    // public LocationDTO Location { get; set; } = new();
+    // public ICollection<Topic> Topics { get; set; } = [];
+    // public ICollection<Class> Classes { get; set; } = [];
+    // public ICollection<Exam> Exams { get; set; } = [];
+    // public ICollection<WaitList> WaitLists { get; set; } = [];
   }
 }
