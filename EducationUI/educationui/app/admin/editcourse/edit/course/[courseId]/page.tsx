1cc7128b545236cbe6349bfcd6e65c381212aef8
Arthur Ortiz <Ortiz.Arthur.E@gmail.com>
2024-05-23 10:25:31 -0600

    Update page.tsx
    Refactored page to query the course within the `EditCourseInfo` controller. This will allow me to re-query the course after a safe for deep comparison.

--- EducationUI/educationui/app/admin/editcourse/edit/course/[courseId]/page.tsx
+++ EducationUI/educationui/app/admin/editcourse/edit/course/[courseId]/page.tsx
@@ -1,15 +1,11 @@
-import { getCourseById } from "@/Utilities/api";
 import EditCourseInfo from "@/app/components/Course/EditCourseInfo";
-import { Course } from "@/app/shared/types/sharedTypes";
 
 const CourseDetail: React.FC<{ params: { courseId: string } }> = async ({ params }) => {
     const courseId = parseInt(params.courseId);
-    const courseJson = await getCourseById(courseId);
-    const course = courseJson as Course;
 
     return (
         <div className='flex justify-center'>
-            <EditCourseInfo course={course} />
+            <EditCourseInfo courseId={courseId}/>
         </div>
   );
 }
