85bf43a235be31e8869ecf92d1f353dbb71ab152
Arthur Ortiz <Ortiz.Arthur.E@gmail.com>
2024-05-30 12:08:23 -0600

    Update EditCourseInfo.tsx

--- EducationUI/educationui/app/components/Course/EditCourseInfo.tsx
+++ EducationUI/educationui/app/components/Course/EditCourseInfo.tsx
@@ -84,10 +84,6 @@ const EditCourseInfo: React.FC<EditCourseInfoProps> = ({courseId : crsId}) => {
 
     // This will sort the classes by date if they are not already sorted
     useEffect(() => { 
-        // if (!courseHasClasses(course.classes)){
-        //     return;
-        // }
-        // console.log("Checking if classes are ordered by date...");  
         if (courseHasClasses(course.classes) && !areClassesOrderedByDate(course.classes)) {
             // console.log("Classes are not ordered by date. Sorting...");
             setCourse(prevCourse => {
@@ -421,14 +417,14 @@ const EditCourseInfo: React.FC<EditCourseInfoProps> = ({courseId : crsId}) => {
                     <div className="navbar-start">
                         <div className="dropdown">
                             <div tabIndex={0} role="button" className="btn btn-circle btn-outline btn-primary text-white lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
                             </div>
                             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                                 {renderNavList()}
                             </ul>
                         </div>
                     </div>
-                    <div className="navbar-center hidden bg-base-300 rounded-xl lg:flex justify-start ">
+                    <div className="navbar-center hidden bg-base-300 rounded-xl lg:flex justify-start">
                         <ul className="menu menu-horizontal space-x-16">
                             {renderNavList()}
                         </ul>
