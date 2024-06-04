391ad79a61690c4bcb134b989611f1a9fd722683
Arthur Ortiz <Ortiz.Arthur.E@gmail.com>
2024-06-04 10:57:24 -0600

    Update CourseCatalog.tsx
    Refactored search to use a button to commence the search

--- EducationUI/educationui/app/components/Course/CourseCatalog.tsx
+++ EducationUI/educationui/app/components/Course/CourseCatalog.tsx
@@ -30,7 +30,7 @@ const CourseCatalog: React.FC<CourseCatalogProps> = ({isAdmin = false}) => {
     useEffect(() => {
         getCourses(isAdmin, selectedStatuses, selectedTopics.map(topic => topic.topicId), searchString);
     }
-    , [selectedStatuses, searchString, selectedTopics]);
+    , [selectedStatuses, selectedTopics]);
 
     useEffect(() => {
         fetchTopics();
@@ -176,6 +176,12 @@ const CourseCatalog: React.FC<CourseCatalogProps> = ({isAdmin = false}) => {
                                     onChange={(e) => setSearchString(e.target.value)} 
                                 />
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                                <button 
+                                    className="btn btn-success btn-sm text-white"
+                                    onClick={() => getCourses(isAdmin, selectedStatuses, selectedTopics.map(topic => topic.topicId), searchString)}
+                                >
+                                    Search
+                                </button>
                             </label>
                         </div>
                     </div>
