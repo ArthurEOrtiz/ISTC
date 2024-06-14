452419d08e6d246000a14c5419e01c5720518c44
Arthur Ortiz <Ortiz.Arthur.E@gmail.com>
2024-06-14 12:12:45 -0600

    Update EditCourseInfo.tsx
    Added Course Action bar.

--- EducationUI/educationui/app/components/Course/EditCourseInfo.tsx
+++ EducationUI/educationui/app/components/Course/EditCourseInfo.tsx
@@ -18,6 +18,7 @@ import { deepEquals } from "@/Utilities/deepEquality";
 import Loading from "@/app/shared/Loading";
 import ExamModal from "../Exam/ExamModal";
 import { areClassesOrderedByDate, courseHasClasses, sortClassesByDate } from "@/Utilities/class";
+import CourseActionBar from "./CourseActionBar";
 
 interface EditCourseInfoProps {
     courseId: number;
@@ -363,14 +364,14 @@ const EditCourseInfo: React.FC<EditCourseInfoProps> = ({courseId : crsId}) => {
                         Delete
                     </button>
                 </li>
-                <li>
+                {/* <li>
                     <button
                         className="text-nowrap text-warning font-bold"
                         onClick={() => console.log(course)}
                     >
                         Log Course
                     </button>
-                </li>
+                </li> */}
             </>
         );
     }  
@@ -408,30 +409,12 @@ const EditCourseInfo: React.FC<EditCourseInfoProps> = ({courseId : crsId}) => {
                 )}
             </div>
             
-            <div className="bg-base-100 shawdow-md rounded-xl p-5 mb-4">
+            <div className="bg-base-100 rounded-xl p-5 mb-4">
                 <div className="mb-2 bg-base-300 rounded-xl p-4">
                     <CourseInfoCard course={course} />
                 </div>
 
-                <div className="navbar">
-                    <div className="navbar-start">
-                        <div className="dropdown">
-                            <div tabIndex={0} role="button" className="btn btn-circle btn-outline btn-primary text-white lg:hidden">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-                            </div>
-                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                                {renderNavList()}
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="navbar-center hidden bg-base-300 rounded-xl lg:flex justify-start">
-                        <ul className="menu menu-horizontal space-x-16">
-                            {renderNavList()}
-                        </ul>
-                    </div>
-                    <div className="navbar-end">
-                    </div>
-                </div>
+                <CourseActionBar navList={renderNavList()} />
             </div>
             
             <div className="space-y-2">
