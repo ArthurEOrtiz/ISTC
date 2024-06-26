45c70233adec3d4e92060f88a4d45106053a9e76
Arthur Ortiz <Ortiz.Arthur.E@gmail.com>
2024-06-26 12:21:22 -0600

    Update page.tsx

--- EducationUI/educationui/app/admin/users/[userId]/page.tsx
+++ EducationUI/educationui/app/admin/users/[userId]/page.tsx
@@ -1,8 +1,4 @@
-
-import UserDashboard from "@/app/components/User/UserDashboard";
-import ErrorModal from "@/app/shared/modals/ErrorModal";
-import { User } from "@/app/shared/types/sharedTypes";
-import { GetUserById } from "@/Utilities/api";
+import EditUser from "@/app/components/User/EditUser";
 
 interface UserPage {
     params: {
@@ -12,21 +8,10 @@ interface UserPage {
 
 const UserPage: React.FC<UserPage> = async ({params}) => {
     const userId = parseInt(params.userId);
-    const user = await GetUserById(userId) as User;
 
     return (
-        <div>
-            {user.clerkId ? (
-                <UserDashboard clerkId={user.clerkId} />
-            ) : (
-                <ErrorModal 
-                title={"User not found"} 
-                message={""} 
-                onClose={function (): void {
-                        throw new Error("User not found");
-                    }}
-                />
-            )}
+        <div className="p-4">
+            <EditUser userId={userId} />
         </div>
     );
 } 
