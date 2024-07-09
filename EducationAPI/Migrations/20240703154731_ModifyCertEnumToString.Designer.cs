d82c86eae9591ce075c6326a4d2dac0e5eaa0876
Arthur Ortiz <Ortiz.Arthur.E@gmail.com>
2024-07-09 09:25:43 -0600

    Migrations
    Added migrations for the certification table

--- EducationAPI/Migrations/20240703154731_ModifyCertEnumToString.Designer.cs
+++ EducationAPI/Migrations/20240703154731_ModifyCertEnumToString.Designer.cs
@@ -4,6 +4,7 @@ using EducationAPI.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EducationAPI.Migrations
 {
     [DbContext(typeof(EducationProgramContext))]
-    partial class EducationProgramContextModelSnapshot : ModelSnapshot
+    [Migration("20240703154731_ModifyCertEnumToString")]
+    partial class ModifyCertEnumToString
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
