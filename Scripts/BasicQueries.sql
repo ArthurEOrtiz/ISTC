819e10f75c185778c04b21d3a5c58319ec1c311f
Arthur Ortiz <Ortiz.Arthur.E@gmail.com>
2024-07-09 09:30:42 -0600

    Update BasicQueries.sql

--- Scripts/BasicQueries.sql
+++ Scripts/BasicQueries.sql
@@ -1,17 +1,25 @@
 SELECT *
 FROM Users;
-----DELETE FROM Users
-----WHERE UserId IN (1031, 1032);
+
 ----UPDATE Users
-----SET IsAdmin = 1;
+----Set IsAdmin = 0
+----WHERE UserId IN (2038, 2039);
 
 SELECT *
 FROM Contacts;
 
 SELECT *
-FROM Students;
-----UPDATE Students
-----Set AccumulatedCredit = 0
+FROM Students
+
+--UPDATE Students
+--SET AppraisalCertified= 0
+--WHERE StudentId = 2034;
+
+--UPDATE Students
+--Set AppraisalCertified = 0
+
+SELECT *
+FROM Certifications;
 
 SELECT *
 FROM Courses;
@@ -19,45 +27,46 @@ FROM Courses;
 SELECT *
 FROM Classes;
 
-
 SELECT *
 FROM Attendances;
-----WHERE class_id IN (158, 159, 160, 161, 165);
-----UPDATE Attendance
-----SET Attended = 1
-----WHERE Attendance.AttendanceId = 1024
+------WHERE class_id IN (158, 159, 160, 161, 165);
+------UPDATE Attendance
+------SET Attended = 1
+------WHERE Attendance.AttendanceId = 1024
 
---SELECT *
---FROM Locations;
+SELECT *
+FROM Locations;
 
-----DELETE FROM Locations
-----WHERE LocationId = 61;
+------DELETE FROM Locations
+------WHERE LocationId = 61;
 
---SELECT *
---FROM WaitLists;
+SELECT *
+FROM WaitLists;
 
---SELECT *
---FROM Exams;
+SELECT *
+FROM Exams;
 
-----DELETE FROM Exams
-----WHERE course_id = 57;
+------DELETE FROM Exams
+------WHERE course_id = 57;
 
---Select * 
---FROM Topics;
+Select * 
+FROM Topics;
 
---SELECT *
---FROM CourseTopic;
+SELECT *
+FROM CourseTopic;
 
---SELECT *
---FROM WaitLists;
+SELECT *
+FROM WaitLists;
 
-----DELETE FROM WaitLists
-----WHERE WaitListId = 18;
+------DELETE FROM WaitLists
+------WHERE WaitListId = 18;
 
---SELECT *
---FROM PDFs;
+----SELECT *
+----FROM PDFs;
 
-----DELETE FROM PDFs
-----WHERE PDFId IN (3,4,5,6,7);
+------DELETE FROM PDFs
+------WHERE PDFId IN (3,4,5,6,7);
+
+
 
 
