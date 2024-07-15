e4a5bdf52f2a2f91dc9b65a07e57d4f5282b1678
Arthur Ortiz <Ortiz.Arthur.E@gmail.com>
2024-07-15 07:47:29 -0600

    Update page.tsx
    changed the syntax of the server side logic, but functionally it does the same.

--- EducationUI/educationui/app/courses/course/[courseId]/page.tsx
+++ EducationUI/educationui/app/courses/course/[courseId]/page.tsx
@@ -1,5 +1,6 @@
 import { getCourseById, GetUserByClerkId } from "@/Utilities/api";
-import UserCourseInfo from "@/app/components/Course/UserCourseInfo";
+import CourseInfo from "@/app/components/Course/CourseInfo";
+import Loading from "@/app/shared/Loading";
 import { Course, User } from "@/app/shared/types/sharedTypes";
 import { currentUser } from "@clerk/nextjs/server";
 
@@ -7,36 +8,29 @@ const CourseDetail: React.FC<{ params: { courseId: string } }> = async ({ params
     const courseId = parseInt(params.courseId);
     const clerkUser = await currentUser();
 
-    const getCourse = async (courseId: number) => {
-        'use server'
-        const response = await getCourseById(courseId);
-        if (response.status === 200) {
-            return response.data as Course;
-        } else {
-            throw new Error("Error fetching course!");
-        }
+    const courseResponse = await getCourseById(courseId);
+
+    if (courseResponse.status !== 200) {
+        throw new Error("Error fetching course!");
     }
 
-    const course: Course | null = await getCourse(courseId);
+    const course = courseResponse.data as Course;
+
+    const userResponse = clerkUser ? await GetUserByClerkId(clerkUser.id) : null;
+
+    if (userResponse && userResponse.status !== 200) {
+        throw new Error("Error fetching user");
+    }
+
+    const user = userResponse ? userResponse.data as User : null;
+
+    if (!course) {
+        return <Loading />;
+    } 
     
-    const getUser = async (clerkId: string | undefined) => {
-        'use server';
-        if (clerkId === undefined) {
-            return null;
-        }
-        const response = await GetUserByClerkId(clerkId);
-        if (response.status === 200) {
-            return response.data as User;
-        } else {
-            throw new Error("Error fetching user");
-        }
-    }
-
-    const user: User | null = await getUser(clerkUser?.id);
-
-   return (
-        <UserCourseInfo course={course} user={user} />
-   );
+    return (
+        <CourseInfo course={course} user={user} />
+    );
 }
 
 export default CourseDetail;
